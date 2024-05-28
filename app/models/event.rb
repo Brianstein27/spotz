9ecaa6dae46a799766cf3a9d09adad820ce5d1e5
Brian Sauls <brianstein27@proton.me>
2024-05-28 14:36:46 +0200

    Add associations and validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :address, presence: true
+  validates :date, presence: true
+  validates :image_url, presence: true
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,8 @@
 class Experience < ApplicationRecord
   has_many :reviews, as :reviewable
+  has_many :spots, through: :links
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 85 }
+  validates :description, length: { maximum: 256 }
 end
