b11c058af65af1deb00ad02ab807ce4f3aac0e14
Brian Sauls <brianstein27@proton.me>
2024-05-30 15:14:40 +0200

    Add option modal on show page

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,24 +1,27 @@
 class VisitsController < ApplicationController
-  def new
-    @user = User.find(params[:user_id])
-    @spot = Spot.find(params[:spot_id])
-    @visit = Visit.new
-  end
-
-  def create
-    @user = User.find(params[:user_id])
-    @spot = Spot.find(params[:spot_id])
-    @visit = Visit.new(visit_params)
-  end
-
-  def destroy
-    @visit = Visit.find(params[:id])
-    @visit.destroy
-  end
-
-  private
-
-  def visit_params
-    params.require(:visit).permit(:spot_id, :user_id)
-  end
+  # probaly not needed
+  #
+  # def new
+  #   @user = User.find(params[:user_id])
+  #   @spot = Spot.find(params[:spot_id])
+  #   @visit = Visit.new
+  # end
+  #
+  # def create
+  #   raise
+  #   @user = User.find(params[:user_id])
+  #   @spot = Spot.find(params[:spot_id])
+  #   @visit = Visit.new(visit_params)
+  # end
+  #
+  # def destroy
+  #   @visit = Visit.find(params[:id])
+  #   @visit.destroy
+  # end
+  #
+  # private
+  #
+  # def visit_params
+  #   params.require(:visit).permit(:spot_id, :user_id)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
-  get 'links/new'
-  get 'links/create'
   devise_for :users
+
   root to: "pages#home"
   get "pages/profile", to: "pages#profile"
 
   resources :bookmarks, only: %i[index destroy]
+  resources :links, only: %i[new crate]
 
   resources :spots, only: %i[index show] do
+    get "create_visit", to: "spots#create_visit"
+    get "options", to: "spots#options"
     resources :bookmarks, only: %i[create]
     resources :reviews, only: %i[new create index edit update destroy]
-    resources :visits, only: %i[new create destroy]
     resources :links, only: %i[new create destroy]
   end
 
   resources :experiences, only: %i[index show] do
     resources :reviews, only: %i[new create index edit update destroy]
-    resources :links, only: [:index, :create, :show]
+    resources :links, only: %i[index create show]
   end
 end
