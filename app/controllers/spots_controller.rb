c4567031a4c9919ab7df5f317b29f923c1464abe
Brian Sauls <brianstein27@proton.me>
2024-06-02 14:47:42 +0200

    Refactor bookmarks, reviews, visits. Add edit and delete methods for reviews.

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -24,40 +24,8 @@ class SpotsController < ApplicationController
   def options
     @visit = Visit.where(user: current_user, spot: params[:spot_id]).first
     @bookmark = Bookmark.where(user: current_user, spot: params[:spot_id]).first
-  end
-
-  def create_visit
-    @visit = Visit.new
-    @spot = Spot.find(params[:spot_id])
-    @visit.user = current_user
-    @visit.spot = @spot
-
-    redirect_to @spot, notice: "spot was added to visited" if @visit.save
-  end
-
-  def delete_visit
-    @spot = Spot.find(params[:spot_id])
-    @visit = Visit.where(user: current_user, spot: @spot).first
-    @visit.delete
-
-    redirect_to @spot, notice: "spot was removed from visited list"
-  end
-
-  def create_bookmark
-    @bookmark = Bookmark.new
-    @spot = Spot.find(params[:spot_id]) if params[:spot_id]
-    @bookmark.user = current_user
-    @bookmark.spot = @spot
-
-    redirect_to @spot, notice: "spot was added to bookmarks" if @bookmark.save
-  end
-
-  def delete_bookmark
-    @spot = Spot.find(params[:spot_id])
-    @bookmark = Bookmark.where(user: current_user, spot: @spot).first
-    @bookmark.delete
-
-    redirect_to @spot, notice: "spot was removed from bookmarks"
+    @review = Review.where(user: current_user, reviewable: params[:spot_id]).first if params[:spot_id]
+    @review = Review.where(user: current_user, reviewable: params[:experience_id]).first if params[:experience_id]
   end
 
   private
