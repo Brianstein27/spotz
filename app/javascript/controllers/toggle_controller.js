bfa0e3b7c4cbb4d3c16420937a048d3a599cc1fc
Precious Kanu <ifechukwukanu@gmail.com>
2024-06-03 12:40:36 +0200

    stimulus toggle refined

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,33 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = [ "visits", "experiences", "reviews" ];
+  static targets = [ "visits", "experiences", "reviews", "visitsHeader", "experiencesHeader", "reviewsHeader" ];
   connect() {
-    console.log("connected");
     this.hideAll()
+    this.uncolorAll()
   }
 
   showVisits() {
     this.hideAll()
+    this.uncolorAll()
     this.visitsTarget.classList.remove("hidden")
-    console.log("visits");
+    this.visitsHeaderTarget.classList.add("text-teal-400")
   }
 
   showExperiences() {
     this.hideAll()
+    this.uncolorAll()
     this.experiencesTarget.classList.remove("hidden")
-    console.log(this.experiencesTarget);
+    this.experiencesHeaderTarget.classList.add("text-teal-400")
   }
 
   showReviews() {
     this.hideAll()
+    this.uncolorAll()
     this.reviewsTarget.classList.remove("hidden")
-    console.log(this.reviewsTarget);
+    this.reviewsHeaderTarget.classList.add("text-teal-400")
   }
 
   hideAll() {
     this.visitsTarget.classList.add("hidden")
     this.experiencesTarget.classList.add("hidden")
     this.reviewsTarget.classList.add("hidden")
+  }
+
+  uncolorAll() {
+    this.visitsHeaderTarget.classList.remove("text-teal-400")
+    this.reviewsHeaderTarget.classList.remove("text-teal-400")
+    this.experiencesHeaderTarget.classList.remove("text-teal-400")
+
   }
 };
